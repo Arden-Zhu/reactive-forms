fd6fbb1c08747dd61dbc8214a3be43bc5b44ab44
Arden Zhu <arden.zhu@lafayette148.com>
2018-06-29 10:40:55 -0400

    use FormBuilder

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,7 +7,15 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
-  heroForm = new FormGroup({
-    name: new FormControl()
-  });
+  heroForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm(): any {
+    this.heroForm = this.fb.group({
+      name : '',
+    })
+  }
 }
