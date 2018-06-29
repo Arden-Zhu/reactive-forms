236a36131097075c8b3b348c16a83711285f4780
Arden Zhu <arden.zhu@lafayette148.com>
2018-06-29 10:27:43 -0400

    Just a form control

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  name = new FormControl();
+}
